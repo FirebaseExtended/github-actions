b4912e0e78ca4b61ab048ca499090d714001011c
Mou <sunmou@google.com>
2022-08-15 15:48:55 -0700

    remove absl-py

--- firebase-test-lab/trigger_ftl_tests.py
+++ firebase-test-lab/trigger_ftl_tests.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 # Copyright 2022 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,17 +19,15 @@ import subprocess
 import threading
 import platform
 import time
+import argparse
+import logging
 
-from absl import app
-from absl import flags
-from absl import logging
 from zipfile import ZipFile
 
 _XCTEST = "xctest"
 _ROBO = "robo"
 _INSTRUMENTATION = "instrumentation"
 _GAMELOOPTEST = "game-loop"
-#TODO: create FTL trigger for android tests.
 
 if platform.system() == 'Windows':
   GCLOUD = "gcloud.CMD"
@@ -37,91 +36,83 @@ else:
   GCLOUD = "gcloud"
   GSUTIL = "gsutil"
 
-FLAGS = flags.FLAGS
+def main():
+  FLAGS = parse_cmdline_args()
 
-flags.DEFINE_string(
-    "project_id", None, "Firebase Project ID.")
-flags.DEFINE_string(
-    "testapp_dir", None,
-    "Testapps (apks and ipas) in this directory will be tested.")
-flags.DEFINE_enum(
-    "test_type", None, [_XCTEST, _ROBO, _INSTRUMENTATION, _GAMELOOPTEST], 
-    "Test type that Firebase Test Lab will run.")
-flags.DEFINE_string(
-    "test_devices", None,
-    "Model id and device version for desired device."
-    "If none, will use FTL's default.")
-flags.DEFINE_string(
-    "timeout", "600s",
-    "Timeout for one ftl test.")
-flags.DEFINE_integer(
-    "retry", 0,
-    "Retry time on failed testapps.")
+  logging.basicConfig(level=logging.DEBUG)
+  logging.getLogger(__name__)
+
+  project_id = _get_project_id(FLAGS.project_id)
+  if not project_id:
+    logging.error("GCLOUD Configuration error: missing project id.")
+    return 1
+
+  testapps = _search_testapps(FLAGS.testapp_dir, FLAGS.test_type)
+  if not testapps:
+    logging.error("No testapps found.")
+    return 1
+
+  logging.info("Sending testapps to FTL")
+  return _run_test_on_ftl(FLAGS, project_id, testapps)
 
 
-def main(argv):
-  if len(argv) > 1:
-    raise app.UsageError("Too many command-line arguments.")
-
-  project_id = FLAGS.project_id
+def _get_project_id(project_id):
   if not project_id:
     project_id = os.getenv('GCLOUD_PROJECT')
-    if not project_id:
-      logging.error("GCLOUD Configuration error: missing project id.")
-      return 1
-  logging.info("project_id: %s", project_id)
 
-  testapp_dir = _fix_path(FLAGS.testapp_dir)
-  testapps=[]
+  logging.info("project_id: %s", project_id)
+  return project_id
+
+
+def _search_testapps(testapp_dir, test_type):
+  testapp_dir = _fix_path(testapp_dir)
+  testapps = []
   for file_dir, _, file_names in os.walk(testapp_dir):
     for file_name in file_names:
       full_path = os.path.join(file_dir, file_name)
-      if FLAGS.test_type==_XCTEST:
+      if test_type==_XCTEST:
         if file_name.endswith(".zip"):
           testapps.append(full_path)
-      elif FLAGS.test_type==_ROBO:
+      elif test_type==_ROBO:
         if file_name.endswith(".apk"):
           testapps.append(full_path)
-      elif FLAGS.test_type==_INSTRUMENTATION:
+      elif test_type==_INSTRUMENTATION:
         if file_name.endswith(".zip"):
           testapps.append(full_path)
-      elif FLAGS.test_type == _GAMELOOPTEST:
+      elif test_type == _GAMELOOPTEST:
         if file_name.endswith(".apk"):
           testapps.append(full_path)
         elif file_name.endswith(".ipa"):
           testapps.append(full_path)
-
-  if not testapps:
-    logging.error("No testapps found.")
-    return 1
   logging.info("Testapps found: %s", testapps)
-
-  logging.info("Sending testapps to FTL")
-  return _run_test_on_ftl(project_id, testapps)
+  return testapps
 
 
-def _run_test_on_ftl(project_id, testapps):
+def _fix_path(path):
+  """Expands ~, normalizes slashes, and converts relative paths to absolute."""
+  return os.path.abspath(os.path.expanduser(path))
+
+
+def _run_test_on_ftl(FLAGS, project_id, testapps):
   threads = []
   tests_result = { "project_id": project_id, "apps": [] }
   for app in testapps:
     logging.info("Start running testapp: %s" % app)
-    thread = threading.Thread(target=_ftl_run, args=(app, tests_result))
+    thread = threading.Thread(target=_ftl_run, args=(FLAGS, app, tests_result))
     threads.append(thread)
     thread.start()
   for thread in threads:
     thread.join()
   return tests_result
 
-def _fix_path(path):
-  """Expands ~, normalizes slashes, and converts relative paths to absolute."""
-  return os.path.abspath(os.path.expanduser(path))
+
 
 # This runs in a separate thread, so instead of returning values we store
 # them as fields so they can be accessed from the main thread.
-def _ftl_run(testapp_path, tests_result):
+def _ftl_run(FLAGS, testapp_path, tests_result):
   """Send the testapp to FTL for testing and wait for it to finish."""
   # These execute in parallel, so we collect the output then log it at once.
-  args = _gcloud_command(testapp_path)
+  args = _gcloud_command(FLAGS, testapp_path)
   
   logging.info("Testapp sent: %s", " ".join(args))
   result = subprocess.Popen(
@@ -177,7 +168,7 @@ def _ftl_run(testapp_path, tests_result):
   tests_result.get('apps').append(test_summary)
 
 
-def _gcloud_command(testapp_path):
+def _gcloud_command(FLAGS, testapp_path):
   """Returns the args to send this testapp to FTL on the command line."""
   test_flags = [
       "--type", FLAGS.test_type,
@@ -232,8 +223,25 @@ def _extract_android_test(zip_path):
             app_path = full_path
   return (app_path, test_path)
 
+def parse_cmdline_args():
+  parser = argparse.ArgumentParser(description='FTL Test trigger.')
+  parser.add_argument('-p', '--project_id',
+    default=None, help='Firebase Project ID..')
+  parser.add_argument('-d', '--testapp_dir',
+    default=None, help='Testapps (apks, ipas, zips) in this directory will be tested.')
+  parser.add_argument('-t', '--test_type',
+    default=None, help='Test type that Firebase Test Lab will run..')
+  parser.add_argument('-m', '--test_devices',
+    default=None, help='Model id and device version for desired device. If none, will use FTL default.')
+  parser.add_argument('-o', '--timeout', 
+    default="600s", help='Timeout for one ftl test.')
+  parser.add_argument('-r', '--retry', 
+    default=0, help='List of operating systems to build for.')
+  parser.add_argument('--log_level', default='info',
+    help="Retry time on failed testapps.")
+  args = parser.parse_args()
+  return args
 
-if __name__ == "__main__":
-  flags.mark_flag_as_required("testapp_dir")
-  app.run(main)
 
+if __name__ == '__main__':
+  main()
